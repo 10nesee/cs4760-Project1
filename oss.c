42b177ce1c8dd939d0884992e6ed859fe7d0a344
10nesee <98235697+10nesee@users.noreply.github.com>
2024-09-11 19:33:40 -0500

    Write code for parse options and recieve the command parameters

--- oss.c
+++ oss.c
@@ -1,7 +1,57 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+// Function to help user run command
+void print_help() {
+    printf("Usage: oss [-h] [-n process] [-s simulation] [-t iteration]\n");
+}
 
 int main(int argc, char *argv[]) {
-	printf("oss file.\n");
-	return 0;
+	int option;
+    	int process = 1;
+    	int simulation = 1;
+    	int iteration = 1;
+
+    	while ((option = getopt(argc, argv, "hn:s:t:")) != -1) {
+        	switch (option) {
+            		case 'h':
+                		print_help();
+                		exit(0);
+            		case 'n':
+                		if (optarg) {
+                    			process = atoi(optarg);
+                		} else {
+                    			fprintf(stderr, "Error: -n option\n"); 
+                    			print_help(); 
+					exit(EXIT_FAILURE); 
+                		}
+                		break;
+            		case 's':
+                		if (optarg) {
+                    			simulation = atoi(optarg);
+                		} else {
+                		    	fprintf(stderr, "Error: -s option\n");
+                    			print_help();
+                    			exit(EXIT_FAILURE);
+                		}
+                		break;
+            		case 't':
+                			if (optarg) {
+                			   	 iteration = atoi(optarg);
+                			} else {
+             					fprintf(stderr, "Error: -t option\n");
+                    				print_help();
+                    				exit(EXIT_FAILURE);
+                			}
+                			break;
+            		default:
+                		print_help();
+                		exit(EXIT_FAILURE);
+        	}
+    	}
+
+    	printf("process=%d\nsimulation=%d\niteration=%d\n", process, simulation, iteration);
+    	return 0;
 }
 
--- user.c
+++ user.c
@@ -1,6 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
 
 int main(int argc, char *argv[]) {
-	printf("user file.\n");
+	if (argc != 2) {
+		fprintf(stderr, "Usage: %s <iterations>\n", argv[0]);
+		exit(EXIT_FAILURE);
+	}
+
+	int iterations = atoi(argv[1]);
+	printf("iterations=%d\n", iterations);
 	return 0;
 }
